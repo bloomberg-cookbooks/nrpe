8ab7deee557bc33e0a52191ea73129cbabda903b
John Bellone <jbellone@bloomberg.net>
2016-08-26 11:02:53 -0400

    Update integration run list for centos/debian.

--- test/fixtures/policies/default.rb
+++ test/fixtures/policies/default.rb
@@ -3,7 +3,7 @@ default_source :community
 default_source :chef_repo, '..'
 cookbook 'nrpe-ng', path: '../../..'
 run_list 'nrpe-ng::default', 'test::default'
-named_run_list :centos, 'sudo::default', 'yum-epel::default', run_list
-named_run_list :debian, 'apt::default', run_list
+named_run_list :centos, 'sudo::default', 'yum::default', 'yum-epel::default', run_list
+named_run_list :debian, 'sudo::default', 'apt::default', run_list
 named_run_list :freebsd, 'freebsd::default', run_list
 named_run_list :windows, 'windows::default', run_list
