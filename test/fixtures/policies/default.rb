6d61915f29900da2a892f836a6d53b6046c73761
Laurie Denness <laurie@denness.net>
2018-10-11 10:28:37 +0100

    Chef 14, fix tests

--- test/fixtures/policies/default.rb
+++ test/fixtures/policies/default.rb
@@ -1,6 +1,9 @@
 name 'default'
 default_source :community
 default_source :chef_repo, '..'
+default_source :chef_repo, ".." do |s|
+  s.preferred_for "test"
+end
 cookbook 'blp-nrpe', path: '../../..'
 run_list 'blp-nrpe::default', 'test::default'
 named_run_list :centos, 'sudo::default', 'yum::default', 'yum-epel::default', run_list
